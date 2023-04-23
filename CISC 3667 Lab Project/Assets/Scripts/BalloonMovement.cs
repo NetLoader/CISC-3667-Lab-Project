4dfd2ebffb8969fe53209f345043df915dd38257
NetLoader <zhengheng9383@live.com>
2023-04-23 18:30:10 -0400

    Added balloon growth, lvl 1 & 2

--- CISC 3667 Lab Project/Assets/Scripts/BalloonMovement.cs
+++ CISC 3667 Lab Project/Assets/Scripts/BalloonMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BalloonMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    private float speed = 5f;
+    private float growthRate = 0.5f;
+    private float maxSize = 13f;
     private Rigidbody2D rb;
+    private float currentSize;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentSize = transform.localScale.x;
     }
 
     void FixedUpdate()
     {
+        if (currentSize >= maxSize)
+        {
+            Destroy(gameObject);
+        }
+        BalloonGrowth();
         rb.velocity = new Vector2(0, speed);
     }
     void OnTriggerEnter2D(Collider2D collision)
@@ -21,5 +30,11 @@ public class BalloonMovement : MonoBehaviour
         {
             speed = -speed;
         }
+    }
+    private void BalloonGrowth()
+    {
+        currentSize += growthRate * Time.deltaTime;
+        currentSize = Mathf.Clamp(currentSize, 0, maxSize);
+        transform.localScale = new Vector3(currentSize, currentSize, currentSize);
     }
 }
